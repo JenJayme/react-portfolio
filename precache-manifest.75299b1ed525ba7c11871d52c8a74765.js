b25c3f60aa7cfab0759df233cb8c982cb068d9e2
Jen Jayme <jeniferjayme@gmail.com>
2020-08-20 03:23:39 -0700

    Updates

---
+++ precache-manifest.75299b1ed525ba7c11871d52c8a74765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f8327d6a12fe2ddf26ec6f5d7a474b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3e1c752ea7d6a4f01cb7",
+    "url": "/react-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e8866087bf6bd8e2c8bc",
+    "url": "/react-portfolio/static/css/main.8160c2a7.chunk.css"
+  },
+  {
+    "revision": "3e1c752ea7d6a4f01cb7",
+    "url": "/react-portfolio/static/js/2.5f074dd6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.5f074dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8866087bf6bd8e2c8bc",
+    "url": "/react-portfolio/static/js/main.c37da78c.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
